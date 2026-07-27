65b8cf393664903892d7678ba7d40c0f449bb349
asidofirus95-max <asidofirus95@gmail.com>
2026-07-27 17:29:25 +0900

    前回コンパイルエラーが起こる状態だったので、引数わすれ、ループの範囲、メソッドのタイポを修正

--- src/traning/Main.java
+++ src/traning/Main.java
@@ -12,14 +12,15 @@ public class Main {
 
     public static void main(String[] args) {
         boolean ran = true;
+        
+        //入力用のスキャナーをインスタンス化
+        Scanner sc = new Scanner(System.in, StandardCharsets.UTF_8);//文字化け対策
+        TrainingService service = new TrainingService();
+        TrainingRepository repository = new TrainingRepository();
         //終了を選択しない限りループする仕様にする
         while(ran){
              try {
-                //入力用のスキャナーをインスタンス化
-                Scanner sc = new Scanner(System.in, StandardCharsets.UTF_8);//文字化け対策
 
-                TrainingService service = new TrainingService();
-                TrainingRepository repository = new TrainingRepository();
             
             
 
@@ -27,8 +28,8 @@ public class Main {
                 System.out.println("1: 手入力");
                 System.out.println("2: ファイル読み込み");
                 System.out.println("3: 登録されている結果の表示");
-                System.out.println("5: 合格していない者の一覧表示");
-                System.out.println("4: 終了");
+                // System.out.println("4: 合格していない者の一覧表示");
+                System.out.println("5: 終了");
 
                 //分岐用の変数
                 String mode = sc.nextLine();
@@ -38,9 +39,11 @@ public class Main {
                          break;
                     case "2": inputFile(sc, service, repository);
                          break;
-                    case "3":printResultList();   
+                    case "3":printResultList(service);   
                         break;
-                    case "4":System.out.println("システムを終了します");
+                    // case "4": printGoukaku(service);//メソッド名仮置き
+                        // break;
+                    case "5":System.out.println("システムを終了します");
                         ran = false;
                         break;
                     default:System.out.println("正しい番号を入力してください");
@@ -52,15 +55,19 @@ public class Main {
                 System.out.println("点数には数値を入力してください");
 
             } catch (IllegalArgumentException e) {
-             System.out.println("入力エラー: " + e.getMessage());
-
+                System.out.println("入力エラー: " + e.getMessage());
+            //ファイルの読み込み失敗時エラーをキャッチできるようにする
+            } catch (IOException e) {
+                System.out.println("ファイルの読み込みに失敗しました。");
+                System.out.println("ファイル名または配置場所を確認してください。");
+            
             } catch (Exception e) {
                 System.out.println("予期しないエラーが発生しました");
-
-            } finally {
-                System.out.println("システムを終了します。");
-                }
+        
+            }
         }
+        System.out.println("プログラムを終了します。");
+        
     }
     //単体読み込み用のメソッド ほかで使わないためprivateに
 
@@ -68,9 +75,9 @@ public class Main {
 
         System.out.println("社員ID、名前、点数の順に入力してください");
         System.out.println("社員ID");
-        String emp_ID = sc.nextLine();
+        String empId = sc.nextLine();
         System.out.println("社員名");
-        String emp_name = sc.nextLine();
+        String empName = sc.nextLine();
         System.out.println("点数");
         //文字列で来たらキャッチする必要性がある
         int score = Integer.parseInt(sc.nextLine());
@@ -78,7 +85,7 @@ public class Main {
         //empをインスタンス化
         Employee emp = new Employee(empId, empName);
         //保存処理に移行
-        saveandplint(emp, score, service, repository);
+        saveandprint(emp, score, service, repository);
     }
 
     //ファイル読み込み用
@@ -101,7 +108,7 @@ public class Main {
                 try {
                     //カンマ区切りで配列に
                     String[] data = line.split(",");
-                    //配列が3つ以下ならエラー処理する
+                    //配列が3つ以外ならエラー処理する
                     if (data.length != 3) {
                         throw new IllegalArgumentException("ファイル形式に問題があります" + line);
                     }
@@ -111,7 +118,7 @@ public class Main {
                     Employee emp = new Employee(empId, empName);
 
                     //All or Nothingにしたかったが、工数がかかるのでで1行づつ保存、結果表示
-                    saveandplint(emp, score, service, repository);
+                    saveandprint(emp, score, service, repository);
 
                 } catch (IllegalArgumentException e) {
                     System.out.println("この行は登録できませんでした: " + line);
@@ -143,7 +150,7 @@ public class Main {
     private static void printResultList(TrainingService service){
         List<TrainingResult> resultList = service.getResultList();
         System.out.println("登録されてる結果を表示します。");
-        if(resultList.isEmpty){
+        if(resultList.isEmpty()){
             System.out.println("登録されている結果がありません");
         }else{
             for (TrainingResult result : resultList) {
