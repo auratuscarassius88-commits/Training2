eae7726a9a0d40a3671f4a33c4730bb4f657fa29
asidofirus95-max <asidofirus95@gmail.com>
2026-08-12 17:29:06 +0900

    8/12追加

--- src/traning/TriningRsultCsvExport.java
+++ src/traning/TriningRsultCsvExport.java
@@ -25,5 +25,43 @@ public class TrainingResultCsvExport{
             
             default:System.out.println("選択肢の数字を選んでください。");
         }
+        //書き込むコードを書く
     }
+
+    public static List<TrainingResult> getFiledResultList(TrainingService service){
+        //登録されている結果を取り出す
+        List<TrainingResult> resultList = service.getResultList();
+        List<TrainingResult> filedList = new ArrayList<>();
+
+        //Mainのメソッド使った方が良くないか？
+        for(TrainingResult result : resultList){
+            if(!"合格".equals(resultList.getJudge())){
+                filedList.add(result);
+            }
+            return filedList;
+        }
+    }
+
+    public static List<TrainingResult> getSearchResultList(TrainingService service, Scanner sc){
+
+        List<TrainingResult> result = service.getResultList();
+        List<TrainingResult> searchList = new ArrayList<>();
+        //検索ワードを入力
+        System.out.println("検索したい社員IDを入力してください");
+        String searchId = sc.nextLine();
+
+        boolean found = false;
+        for(TrainingResult result :resultList){
+            if(searchId.equals(result.getEmp().getEmpId())){
+                printResult(result);
+                found = true;
+                //IDが一意な為、見つけることができたら処理から脱出
+                break; 
+            }
+        }
+        if(found == false){
+            System.out.println(searchId + "は存在しません");
+        }
+    }
+
 }
